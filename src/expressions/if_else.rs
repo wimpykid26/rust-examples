9c5b92df9752036617bdf14175fb2cc05db16b94
Saha <dwaipayan.saha@sap.com>
2018-08-25 20:49:06 +0530

    Added if-else basic

--- src/expressions/if_else.rs
+++ src/expressions/if_else.rs
@@ -1,3 +1,13 @@
 pub fn test() {
-    
+    let n = 5;
+    if n > 0 {
+        println!("{} is positive", n);
+    } else if n < 0 {
+        println!("{} is negative", n);
+    } else {
+        println!("{} is zero", n);
+    }
+
+    let big_n = if n > 0 { n * 10 } else { 0 };
+    println!("{} - {}", n, big_n);
 }
